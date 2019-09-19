08d20a955b1a3c9ad78c594c10efc90f1f4aa570
Arjit <arjit.sharma@gmail.com>
2019-09-19 20:12:43 +0100

    initial commit

---
+++ SecureVideoTransfer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SecureVideoTransfer
+//
+//  Created by Arjit Sharma on 19/09/2019.
+//  Copyright © 2019 Arjit Sharma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
